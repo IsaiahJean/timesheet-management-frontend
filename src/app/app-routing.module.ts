34868a80472acd88aa9d3023e99fcb2441b82bb1
Owner <icjean973@gmail.com>
2019-09-25 13:36:32 -0400

    'merge'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdministrationComponent } from './administration/administration.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: AdministrationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
